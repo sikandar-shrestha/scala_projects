7ccd6ab3e9b8ada00e82ff4dd814d92334c56e92
sikandar-shrestha <sikandarshrestha@gmail.com>
2024-04-27 13:12:50 -0500

    updated

--- sess9mode.scala
+++ sess9mode.scala
@@ -24,21 +24,67 @@ object sess9mode {
 					import spark.implicits._
 					
 					
+/*
+ * 
+------------------------------
+Test each Mode separately 
+-----------------------------
+
+val df = spark.read.format("json").load("file:///C:/data/devices.json")
+
+val finaldf = df.filter("humidity>40")
+
+==========
+Run it  ---- It would be successful
+==========
+finaldf.write.format("csv").mode("error").save("file:///C:/data/dhumid")
+
+
+==========
+Run it  --- It will through Error
+==========
+
+finaldf.write.format("csv").mode("error").save("file:///C:/data/dhumid")
+
+==========
+Run it  --- it will append the data
+==========
+
+finaldf.write.format("csv").mode("append").save("file:///C:/data/dhumid")
+
+==========
+Run it  --- it will overwrite the data
+==========
+
+finaldf.write.format("csv").mode("overwrite").save("file:///C:/data/dhumid")
+
+==========
+Run it  --- it will ignore the data
+==========
+
+finaldf.write.format("csv").mode("ignore").save("file:///C:/data/dhumid")
+ * 
+ */
+					
+/*     
+ *     In summary:----   
+ *          finaldf.write.format("csv").mode("error").save("file:///C:/data/dhumid")
+ *          finaldf.write.format("csv").mode("append").save("file:///C:/data/dhumid")
+ *          finaldf.write.format("csv").mode("overwrite").save("file:///C:/data/dhumid")          
+ */					
 					
 				
-					
+           println("===== dataframe of file(devices.json) =====")					
 					val df = spark.read.format("json").load("file:///C:/data/devices.json")
 					df.show()
 					
+					println("===== dataframe of file(devices.json) where humidity>40 =====")
           val finaldf = df.filter("humidity>40")
           finaldf.show()
           
-          /*         
-           *          finaldf.write.format("csv").mode("error").save("file:///C:/data/dhumid")
-           *          finaldf.write.format("csv").mode("append").save("file:///C:/data/dhumid")
-           *          finaldf.write.format("csv").mode("overwrite").save("file:///C:/data/dhumid")          
-           */
-
+          
+          
+          println("=== dhumid write on c:/data/ in ignore mode ======")
           finaldf.write.format("csv").mode("ignore").save("file:///C:/data/dhumid")
 
 					println("====mode ignore======")
